c50b7299cf0eb7b101c441638d8f9ffb4d50c2ec
jorgeemzu <jorge.emzu@gmail.com>
2021-02-01 21:34:54 -0400

    se crearon las paginas para cada uno de los servicios

--- app.js
+++ app.js
@@ -6,23 +6,28 @@ const dropDownArrow = document.querySelector('.mobile__menu--item i')
 const mapTiles = "http://{s}.tile.osm.org/{z}/{x}/{y}.png";
 
 
+
 hamburger.addEventListener('click', () => {
         mobileMenu.classList.toggle('show__menu');
         console.log('owrk')
 })
+
+
+
 
 servicesBtn.addEventListener('click', () => {
         dropdownMobile.classList.toggle('show__dropdown--mobile')
         dropDownArrow.classList.toggle('rotate__arrow')
 })
 
+
+
+
+
 let myMap = L.map('map').setView([29.742026, -95.457421], 16);
 
 L.tileLayer(mapTiles ,{
-	maxZoom: 18,
+        maxZoom: 18,
 }).addTo(myMap);
 
 let marker = L.marker([29.742026, -95.457421]).addTo(myMap);
-
-
-
